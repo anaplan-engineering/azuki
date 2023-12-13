8c6faf3a87f1522c6131d17a05fe63fad1adac5d
Simon Fraser <simon.fraser@anaplan.com>
2023-12-13 13:20:08 +0000

    Added implementation-specific version filtering

--- tictactoe/adapter-vdm/src/main/kotlin/com/anaplan/engineering/azuki/tictactoe/adapter/vdm/VdmTicTacToeImplementation.kt
+++ tictactoe/adapter-vdm/src/main/kotlin/com/anaplan/engineering/azuki/tictactoe/adapter/vdm/VdmTicTacToeImplementation.kt
@@ -1,5 +1,6 @@
 package com.anaplan.engineering.azuki.tictactoe.adapter.vdm
 
+import com.anaplan.engineering.azuki.core.system.Implementation
 import com.anaplan.engineering.azuki.core.system.NoSystemDefaults
 import com.anaplan.engineering.azuki.tictactoe.adapter.api.TicTacToeImplementation
 
@@ -8,6 +9,8 @@ class VdmTicTacToeImplementation: TicTacToeImplementation {
     override val name = "VDM"
 
     override val implementationDefaults = NoSystemDefaults
+
+    override val versionFilter = Implementation.VersionFilter.DefaultVersionFilter
 
     override fun createSystemFactory(systemDefaults: NoSystemDefaults) = VdmSystemFactory()
 }
