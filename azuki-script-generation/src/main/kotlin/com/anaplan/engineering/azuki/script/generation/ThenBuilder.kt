b5ce85e470ed5429760e53391638b3f665da2911
Matt Windsor <matt.windsor@anaplan.com>
2025-07-08 09:52:41 +0100

    Added a comment for ThenBuilder

--- azuki-script-generation/src/main/kotlin/com/anaplan/engineering/azuki/script/generation/ThenBuilder.kt
+++ azuki-script-generation/src/main/kotlin/com/anaplan/engineering/azuki/script/generation/ThenBuilder.kt
@@ -4,6 +4,14 @@ import com.anaplan.engineering.azuki.core.system.Check
 import com.anaplan.engineering.azuki.core.system.UnsupportedCheck
 import org.slf4j.LoggerFactory
 
+/**
+ * Builder for the contents of a 'then' block in a generated script.
+ *
+ * Implementations of this interface receive the low-level checks passed into
+ * script generation from the scenario or system.  They can group together
+ * checks, rewrite them, reorder them, and perform other such preprocessing,
+ * to reconstitute the high-level DSL.
+ */
 interface ThenBuilder {
     fun addCheck(check: Check)
 
