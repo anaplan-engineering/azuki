f1a02825463616ed192233ff4d74c1032e1ba084
Matt Windsor <matt.windsor@anaplan.com>
2025-11-17 17:19:36 +0000

    Remove unused import

--- azuki-script-generation/src/main/kotlin/com/anaplan/engineering/azuki/script/generation/ScriptGenerationActionGenerator.kt
+++ azuki-script-generation/src/main/kotlin/com/anaplan/engineering/azuki/script/generation/ScriptGenerationActionGenerator.kt
@@ -1,7 +1,6 @@
 package com.anaplan.engineering.azuki.script.generation
 
 import com.anaplan.engineering.azuki.core.system.ActionGenerator
-import kotlin.reflect.KFunction
 
 fun interface ScriptGenerationActionGenerator: ActionGenerator {
 
