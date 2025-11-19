4090455181579dadf8ceeabe5d1b324cb88cf763
Matt Windsor <matt.windsor@anaplan.com>
2025-11-19 09:05:55 +0000

    Revert "Remove unused system writer"
    This reverts commit c124555ab981e75e5a8b3da98e0f74b19dbe5640.

--- tictactoe/adapter-script-generation/src/main/kotlin/com/anaplan/engineering/azuki/tictactoe/adapter/scriptgen/TicTacToeScriptGenerator.kt
+++ tictactoe/adapter-script-generation/src/main/kotlin/com/anaplan/engineering/azuki/tictactoe/adapter/scriptgen/TicTacToeScriptGenerator.kt
@@ -9,6 +9,7 @@ import com.anaplan.engineering.azuki.tictactoe.adapter.api.TicTacToeQueryFactory
 import com.anaplan.engineering.azuki.tictactoe.adapter.declaration.TicTacToeDeclarationState
 import kotlin.Result.Companion.failure
 import kotlin.Result.Companion.success
+import kotlin.reflect.KFunction
 
 class TicTacToeScriptGenerator(environment: TicTacToeGenerationEnvironment = TicTacToeGenerationEnvironment()) :
     VerificationCapableScriptGenerator<TicTacToeActionFactory, TicTacToeCheckFactory, TicTacToeQueryFactory, TicTacToeActionGeneratorFactory, TicTacToeDeclarationState, TicTacToeGenerationEnvironment>(
