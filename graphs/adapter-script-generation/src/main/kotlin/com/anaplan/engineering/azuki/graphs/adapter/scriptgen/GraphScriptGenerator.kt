5cb1348d2b184fe3377176dc486e3190ac67f1f0
Matt Windsor <matt.windsor@anaplan.com>
2025-11-20 11:02:29 +0000

    Remove reference to GraphScriptGenerator

--- graphs/adapter-script-generation/src/main/kotlin/com/anaplan/engineering/azuki/graphs/adapter/scriptgen/GraphScriptGenerator.kt
+++ graphs/adapter-script-generation/src/main/kotlin/com/anaplan/engineering/azuki/graphs/adapter/scriptgen/GraphScriptGenerator.kt
@@ -1,26 +1,12 @@
 package com.anaplan.engineering.azuki.graphs.adapter.scriptgen
 
-import com.anaplan.engineering.azuki.core.system.NoActionGeneratorFactory
-import com.anaplan.engineering.azuki.core.system.NoQueryFactory
-import com.anaplan.engineering.azuki.graphs.adapter.api.GraphActionFactory
-import com.anaplan.engineering.azuki.graphs.adapter.api.GraphCheckFactory
 import com.anaplan.engineering.azuki.graphs.adapter.declaration.GraphDeclarationState
-import com.anaplan.engineering.azuki.script.generation.NoScriptGenerationEnvironment
-import com.anaplan.engineering.azuki.script.generation.ScriptGenerator
 import com.anaplan.engineering.azuki.script.generation.ScriptGenerationService
 import com.anaplan.engineering.azuki.script.generation.ScriptingHelper
 
 val GraphScriptGeneration = ScriptGenerationService.create(GraphScriptGenerationActionFactory,
     GraphScriptGenerationCheckFactory,
     ::GraphDeclarationState)
-
-object GraphScriptGenerator :
-    ScriptGenerator<GraphActionFactory<*>, GraphCheckFactory, NoQueryFactory, NoActionGeneratorFactory, GraphDeclarationState, NoScriptGenerationEnvironment>(
-        GraphScriptGenerationActionFactory,
-        GraphScriptGenerationCheckFactory,
-        ::GraphDeclarationState,
-        NoScriptGenerationEnvironment,
-    )
 
 val GraphScriptingHelper = ScriptingHelper(mapOf(
     String::class to { v: Any? -> "\"${v.toString()}\"" },
