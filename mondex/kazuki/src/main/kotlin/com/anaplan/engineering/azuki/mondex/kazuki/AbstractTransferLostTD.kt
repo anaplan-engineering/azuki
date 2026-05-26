58bb7738ecb40ca03a06a902989392dd294beb5d
erin-kingston <erin.kingston@anaplan.com>
2026-05-26 11:21:03 +0100

    SYSS-1140 Add last bits of Abstract spec

---
+++ mondex/kazuki/src/main/kotlin/com/anaplan/engineering/azuki/mondex/kazuki/AbstractTransferLostTD.kt
@@ -0,0 +1,8 @@
+package com.anaplan.engineering.azuki.mondex.kazuki
+
+import com.anaplan.engineering.kazuki.core.*
+
+class AbstractTransferLostTD(world: Delta<World>, aQ: AIN, aE: AOUT, transferDetails: TransferDetails):
+    AbstractWorldSecureOperation(world, aQ, aE, transferDetails) {
+
+}
