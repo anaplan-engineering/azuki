51710907192d6a4531256c42e2f447dde2894d65
Leo Freitas <leo.freitas@anaplan.com>
2026-05-30 11:28:55 +0100

    SYSS-1103 RightOfWay up to Thm1

--- right-of-way/kauzki/src/main/kotlin/com/anaplan/engineering/azuki/rightofway/kazuki/RightOfWay.kt
+++ right-of-way/kauzki/src/main/kotlin/com/anaplan/engineering/azuki/rightofway/kazuki/RightOfWay.kt
@@ -1,61 +1,85 @@
 package com.anaplan.engineering.azuki.rightofway.kazuki
 
-//import com.anaplan.engineering.azuki.rightofway.kazuki.RightOfWay_Module.mk_Game
-//import com.anaplan.engineering.azuki.rightofway.kazuki.RightOfWay_Module.mk_RVector
-import com.anaplan.engineering.azuki.rightofway.kazuki.RightOfWay.dot_product
-import com.anaplan.engineering.azuki.rightofway.kazuki.RightOfWay.to_the_right_of
-import com.anaplan.engineering.azuki.rightofway.kazuki.RightOfWay_Module.mk_RVector
+import com.anaplan.engineering.azuki.rightofway.kazuki.Position_Module.as_Position
+import com.anaplan.engineering.azuki.rightofway.kazuki.RVector_Module.mk_RVector
+import com.anaplan.engineering.azuki.rightofway.kazuki.RightOfWay.orientation
+import com.anaplan.engineering.azuki.rightofway.kazuki.Velocity_Module.as_Velocity
 import com.anaplan.engineering.kazuki.core.*
+import kotlin.math.atan
+import kotlin.math.sqrt
+
+const val DEFAULT_SQRT_ERROR: PNZReal = 0.000001;
+
+@PrimitiveInvariant(name = "Real", base = Double::class)
+fun isReal(r: Double) = !r.isNaN() && !r.isInfinite()
+
+@PrimitiveInvariant(name = "PReal", base = Double::class)
+fun isPReal(r: Double) = isReal(r) && r >= 0.0
+
+@PrimitiveInvariant(name = "NZReal", base = Double::class)
+fun isNZReal(r: Double) = isReal(r) && r != 0.0
+
+@PrimitiveInvariant(name = "NReal", base = Double::class)
+fun isNReal(r: Double) = isReal(r) && r < 0.0
+
+//    @PrimitiveInvariant(name = "PNZReal", base = PReal::class)
+@PrimitiveInvariant(name = "PNZReal", base = Double::class)
+fun isPNZReal(r: Double) = isPReal(r) && isNZReal(r)
+
+@PrimitiveInvariant(name = "NNZReal", base = Double::class)
+fun isNNZReal(r: Double) = isNReal(r) && isNZReal(r)
+
+@PrimitiveInvariant(name = "Angle", base = Double::class)
+fun isAngle(r: Double) = isPReal(r) && r <= 360.0
+
+@Module
+interface RVector {
+//    val x: Real
+//    val y: Real
+//    [ksp] java.lang.IllegalArgumentException: Error type '<ERROR TYPE: Real>' is not resolvable in the current round of processing.
+//    at com.squareup.kotlinpoet.ksp.KsTypesKt.toTypeName(KsTypes.kt:61)
+
+    val x: Double
+    val y: Double
+
+    fun isReal() = isReal(x) && isReal(y)
+}
+
+// aircraft position
+@Module
+interface Position : RVector {
+    @Invariant
+    fun isPReal() = isPReal(x) && isPReal(y)
+}
+
+// aircraft velocity
+@Module
+interface Velocity : RVector {
+    @Invariant
+    fun isPNZReal() = isPNZReal(x) && isPNZReal(y)
+}
 
 @Module
 object RightOfWay {
-    const val DEFAULT_SQRT_ERROR: PNZReal = 0.000001;
 
-    @PrimitiveInvariant(name = "Real", base = Double::class)
-    fun isReal(r: Double) = !r.isNaN() && !r.isInfinite()
-
-    @PrimitiveInvariant(name = "PReal", base = Double::class)
-    fun pReal(r: Double) = isReal(r) && r >= 0.0
-
-    @PrimitiveInvariant(name = "NZReal", base = Double::class)
-    fun nzReal(r: Double) = isReal(r) && r != 0.0
-
-    @PrimitiveInvariant(name = "NReal", base = Double::class)
-    fun nReal(r: Double) = isReal(r) && r < 0.0
-
-//    @PrimitiveInvariant(name = "PNZReal", base = PReal::class)
-    @PrimitiveInvariant(name = "PNZReal", base = Double::class)
-    fun pnzReal(r: Double) = pReal(r) && nzReal(r)
-
-    @PrimitiveInvariant(name = "NNZReal", base = Double::class)
-    fun nnzReal(r: Double) = nReal(r) && nzReal(r)
-
-    @PrimitiveInvariant(name = "Angle", base = Double::class)
-    fun planeAngle(r: Double) = pReal(r) && r < 360.0
-
-    interface RVector {
-        val x: Double
-        val y: Double
-
-        @Invariant
-        fun isReal() = isReal(x) && isReal(y)
-    }
-
-    // plane position
-    interface Position {
-        val x: PReal
-        val y: PReal
-        val other: integer
-    }
-
-    //typealias Position = RVector + PReal(x) + PReal(y)
-
-    // plane velocity
-    interface Velocity {
-        val x: PNZReal
-        val y: PNZReal
-        val dummy: bool // to disambiguate on the Velocity_Rec x Position_Rec :-(
-    }
+//    interface RVector {
+//        val x: Double
+//        val y: Double
+//
+//            @Invariant
+//            fun isReal() = isReal(x) && isReal(y)
+//        }
+//    // plane position
+//    interface Position {
+//        val x: PReal
+//        val y: PReal
+//        val other: integer
+//    }
+    //    interface Velocity {
+//        val x: PNZReal
+//        val y: PNZReal
+//        val dummy: bool // to disambiguate on the Velocity_Rec x Position_Rec :-(
+//    }
 
 //    interface Velocity : Position {
 //        val dummy: Boolean // must have fields error
@@ -107,7 +131,11 @@ object RightOfWay {
         },
         // no need for pre given the invariant of RVector?
         pre = { u, v -> u.isReal() && v.isReal() },
-        post = { u, v, _ ->
+        post = { u, v, r ->
+            (isZeroVector(u) && isZeroVector(v) implies (r == 0.0))
+                &&
+            (u == v && !isZeroVector(u) implies isNZReal(r))
+                &&
             // (u . v)^2 <= (u . u) * (v . v)
             (u.x*v.x + u.y*v.y) * (u.x*v.x + u.y*v.y) <=
             (u.x*u.x + u.y*u.y) * (v.x*v.x + v.y*v.y)
@@ -120,11 +148,27 @@ object RightOfWay {
         }
     )
 
+    val magnitude = function(
+        command = { u: RVector ->
+            sqrt(u.x * u.x + u.y * u.y)
+            //sqrt'(u.x * u.x + u.y * u.y, DEFAULT_SQRT_ERROR)
+        }
+    )
+
     //TODO when to use a function like this or an @FunctionProvider??
+
+    // Aircraft quadrant relative to plane
+    // 1: front right
+    // 2: front left
+    // 3: behind left
+    // 4: behind right
     val Q1 = function(
         command = { a: Aircraft, p: Position ->
-            val psub = subtractVectors(p, a.position)
-            dot_product(psub, rotate90(a.velocity)) > 0.0
+            // refactor to avoid repetition
+            //val psub = subtractVectors(p, a.position) as Position
+            val psub = as_Position(subtractVectors(p, a.position))
+            val vrot = as_Velocity(rotate90(a.velocity))
+            dot_product(psub, vrot) > 0.0
                 &&
                 dot_product(psub, a.velocity) >= 0.0
         },
@@ -133,11 +177,88 @@ object RightOfWay {
         }
     )
 
-//    val Q2 = function(
-//        command = { }
-//    )
+    val Q2 = function(
+        command = { a: Aircraft, p: Position ->
+            val psub = as_Position(subtractVectors(p, a.position))
+            val vrot = as_Velocity(rotate90(a.velocity))
+            dot_product(psub, vrot) <= 0.0
+                &&
+                dot_product(psub, a.velocity) > 0.0
 
+        }
+    )
 
+    val Q3 = function(
+        command = { a: Aircraft, p: Position ->
+            val psub = as_Position(subtractVectors(p, a.position))
+            val vrot = as_Velocity(rotate90(a.velocity))
+            dot_product(psub, vrot) < 0.0
+                &&
+                dot_product(psub, a.velocity) <= 0.0
+        }
+    )
+
+    val Q4 = function(
+        command = { a: Aircraft, p: Position ->
+            val psub = as_Position(subtractVectors(p, a.position))
+            val vrot = as_Velocity(rotate90(a.velocity))
+            dot_product(psub, vrot) >= 0.0
+                &&
+                dot_product(psub, a.velocity) < 0.0
+        }
+    )
+
+    // Aircrafrt track (i.e. angle between north and aircraft directon)
+    val track = function(
+        command = { a: Aircraft, p: Position ->
+            // a.velocity.y is NZReal
+            //TODO: how to "cast" the result type to impose invariant?
+            atan(a.velocity.x / a.velocity.y) as Angle
+        },
+        post = { _, _, r -> isAngle(r) }
+    )
+
+    // Time to Closest Point of Approach
+    val tCPA = function( //: VFunction2<RightOfWay.Aircraft, RightOfWay.Aircraft, out Double>
+        command = { a1: Aircraft, a2: Aircraft ->
+            if (a1.velocity == a2.velocity)
+                0.0 // if 0, type gets captured as Number!!!!
+            else {
+                val pDiff = as_Position(subtractVectors(a1.position, a2.position))
+                val vDiff = as_Velocity(subtractVectors(a1.velocity, a2.velocity))
+                // Because Velocity are different, then their difference is not zero
+                val vDiffProd = dot_product(vDiff, vDiff)
+                (- ( dot_product(pDiff, vDiff) / vDiffProd)) as Double // why isn't result double? as Double
+            }
+        },
+        //pre fails for example!
+        post = { _, _, r -> isNZReal(r) }
+    )
+
+    // Horizontal Miss Distance is the distance at the Closest Point of Approach
+    // when their position and velocity vectors are projected in time.
+    val horizontalMissDistance = function(
+        command = { a1: Aircraft, a2: Aircraft ->
+            magnitude(
+                sumVectors(
+                    subtractVectors(a1.position, a2.position),
+                    scalar_product(
+                        tCPA(a1, a2),
+                        subtractVectors(a1.velocity, a2.velocity)
+                    )
+                )
+            )
+        }
+    )
+
+    // Aircraft relative position
+    val to_the_left_of = function(
+        command = { a: Aircraft, p: Position ->
+            dot_product(
+                subtractVectors(p, a.position),
+                rotate90(a.velocity)) < 0.0
+        }
+    )
 
     val to_the_right_of = function(
         command = { a: Aircraft, p: Position ->
@@ -147,6 +268,104 @@ object RightOfWay {
         }
     )
 
+    // Aircraft relative velocity/motion
+    val left_to_right = function(
+        command = { a1: Aircraft, a2: Aircraft ->
+            dot_product(a1.velocity, a2.velocity) < 0.0
+        }
+    )
+
+    val right_to_left = function(
+        command = { a1: Aircraft, a2: Aircraft ->
+            dot_product(a1.velocity, a2.velocity) > 0.0
+        }
+    )
+
+    // Check trajectories will cross in future
+    val going_to_cross = function(
+        command = { a1: Aircraft, a2: Aircraft ->
+            to_the_left_of(a1, a2.position) && left_to_right(a1, a2)
+                ||
+                to_the_right_of(a1, a2.position) && right_to_left(a1, a2)
+        }
+    )
+
+    // Check trajectories have already crossed
+    val crossed = function(
+        command = { a1: Aircraft, a2: Aircraft ->
+            to_the_left_of(a1, a2.position) && right_to_left(a1, a2)
+                ||
+                to_the_right_of(a1, a2.position) && left_to_right(a1, a2)
+        }
+    )
+
+    // Check trajectories are going to cross but have not yet crossed
+    val zero_crossed = function(
+        command = { a1: Aircraft, a2: Aircraft ->
+            going_to_cross(a1, a2) && going_to_cross(a2, a1)
+        }
+    )
+
+    // One aircraft (a1) has crossed trajectory of other (a2) but not other way round
+    val one_crossed = function(
+        command = { a1: Aircraft, a2: Aircraft ->
+            going_to_cross(a1, a2) && crossed(a1, a2)
+                ||
+                going_to_cross(a2, a1) && crossed(a2, a1)
+        }
+    )
+
+    // both aircraft crossed each other's trajectories
+    val both_crossed = function(
+        command = { a1: Aircraft, a2: Aircraft ->
+            crossed(a1, a2) && crossed(a2, a1)
+        }
+    )
+
+    val orientation = function(
+        command = { a1: Aircraft, a2: Aircraft ->
+            dot_product(a1.velocity, rotate90(a2.velocity)) //as Angle
+        },
+        //post = { _, _, r -> isAngle(r) }
+    )
+    val parallel = function(
+        command = { a1: Aircraft, a2: Aircraft ->
+            orientation(a1, a2) == 0.0
+        }
+    )
+
+    val same_orientation = function(
+        command = { a1: Aircraft, a2: Aircraft ->
+            orientation(a1, a2) > 0.0
+        }
+    )
+
+    val opposite_orientation = function(
+        command = { a1: Aircraft, a2: Aircraft ->
+            orientation(a1, a2) < 0.0
+        }
+    )
+
+    val isInQ1andWasInQ2 = function(
+        command = { a1: Aircraft, a2: Aircraft ->
+            Q1(a1, a2.position) && Q1(a2, a1.position)
+                ||
+                (opposite_orientation(a1, a2) and left_to_right(a1, a2));
+        }
+    )
+
+    // Quadrant convergence
+    val QC = function(
+        command = { a1: Aircraft, a2: Aircraft ->
+            Q1(a1, a2.position) && Q1(a2, a1.position)
+                ||
+                Q2(a1, a2.position) && Q2(a2, a1.position)
+                ||
+                Q3(a1, a2.position) && Q3(a2, a1.position)
+                ||
+                Q4(a1, a2.position) && Q4(a2, a1.position)
+        }
+    )
 }
 
 //
