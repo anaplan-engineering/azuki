81f654bcb3be90be6723830ff106dc34e815c0d9
erin-kingston <erin.kingston@anaplan.com>
2026-06-30 12:41:24 +0100

    Finish ConPurse operations

--- mondex/kazuki/src/main/kotlin/com/anaplan/engineering/azuki/mondex/kazuki/betw/ConPurse.kt
+++ mondex/kazuki/src/main/kotlin/com/anaplan/engineering/azuki/mondex/kazuki/betw/ConPurse.kt
@@ -4,16 +4,7 @@ import com.anaplan.engineering.azuki.mondex.kazuki.Name
 import com.anaplan.engineering.azuki.mondex.kazuki.betw.ConPurse_Module.transform
 import com.anaplan.engineering.azuki.mondex.kazuki.betw.CounterPartyDetails_Module.mk_CounterPartyDetails
 import com.anaplan.engineering.azuki.mondex.kazuki.betw.PayDetails_Module.mk_PayDetails
-import com.anaplan.engineering.kazuki.core.FunctionProvider
-import com.anaplan.engineering.kazuki.core.Invariant
-import com.anaplan.engineering.kazuki.core.Module
-import com.anaplan.engineering.kazuki.core.as_Set
-import com.anaplan.engineering.kazuki.core.forall
-import com.anaplan.engineering.kazuki.core.function
-import com.anaplan.engineering.kazuki.core.implies
-import com.anaplan.engineering.kazuki.core.mk_
-import com.anaplan.engineering.kazuki.core.mk_Set
-import com.anaplan.engineering.kazuki.core.nat
+import com.anaplan.engineering.kazuki.core.*
 
 //LF Perhaps reuse from adapter-api?
 enum class Status { eaFrom, eaTo, epr, epv, epa }
@@ -253,6 +244,283 @@ class ConPurseFunctions(private val old: ConPurse) {
             // check abort post from start to middle; check startFrom post from middle to dash
             abortPurseOkay.post(m, mk_(middle, mm)) &&
                 middle.functions.startFromPurseEaFromOkay.post(m, conjuredUpCPD(), result)
+        }
+    )
+
+    val validStartTo = function(
+        command = { m: Message, cpd: CounterPartyDetails ->
+            old
+        },
+        pre = { m, cpd ->
+            m == Message.StartTo(cpd)
+                && cpd.name != old.name
+        },
+    )
+
+    val startToPurseEaFromOkay = function(
+        command = { m: Message, cpd: CounterPartyDetails ->
+            val dash = validStartTo(m, cpd)
+            val newPdAuth = mk_PayDetails(
+                to = old.name,
+                from = cpd.name,
+                value = cpd.value,
+                toSeqNo = old.nextSeqNo,
+                fromSeqNo = cpd.nextSeqNo,
+            )
+            mk_(old.transform(
+                nextSeqNo = old.nextSeqNo + 1UL,
+                status = Status.epv,
+                pdAuth = newPdAuth
+            ),
+                Message.Req(newPdAuth))
+        },
+        pre = { m, cpd ->
+            old.pdAuth != null &&
+                validStartTo.pre(m, cpd) &&
+                old.status == Status.eaFrom &&
+                old.nextSeqNo < MAX_NAT &&
+                cpd.nextSeqNo < old.nextSeqNo
+        },
+        post = { m, cpd, result ->
+            val (dash, mr) = result
+            xiConPurseStart(old, dash) &&
+                dash.nextSeqNo > old.nextSeqNo &&
+                dash.pdAuth!!.to == old.name &&
+                dash.pdAuth!!.from == cpd.name &&
+                dash.pdAuth!!.value == cpd.value &&
+                dash.pdAuth!!.toSeqNo == old.nextSeqNo &&
+                dash.pdAuth!!.fromSeqNo == cpd.nextSeqNo &&
+                dash.status == Status.epv &&
+                mr == Message.Req(dash.pdAuth!!)
+        }
+    )
+
+    val startToPurseOkay = function(
+        command = { m: Message ->
+            val (dash, _) = abortPurseOkay(m)
+            mk_(dash.functions.startToPurseEaFromOkay(m, conjuredUpCPD()), Message.Bottom)
+        },
+        pre = { m ->
+            abortPurseOkay.pre(m) &&
+                startToPurseEaFromOkay.pre(m, conjuredUpCPD())
+        },
+        post = { m, result ->
+            val (middle, mm) = abortPurseOkay(m)
+            val (dash, mr) = result
+            abortPurseOkay.post(m, mk_(middle, mm)) &&
+                middle.functions.startToPurseEaFromOkay.post(m, conjuredUpCPD(), dash)
+
+        }
+    )
+
+    val authenticReqMessage = function(
+        command = { m: Message ->
+            old
+        },
+        pre = { m ->
+            m == Message.Req(old.pdAuth!!)
+        },
+    )
+
+    private val xiConPurseReq = function(
+        command = { before: ConPurse, after: ConPurse ->
+            //before.balance == after.balance &&
+                before.exLog == after.exLog &&
+                before.name == after.name &&
+                before.nextSeqNo == after.nextSeqNo &&
+                before.pdAuth == after.pdAuth //&&
+                //before.status == after.status
+        }
+    )
+
+    val reqPurseOkay = function(
+        command = { m: Message ->
+            val dash = authenticReqMessage(m)
+            mk_(old.transform(
+                balance = old.balance - old.pdAuth!!.value,
+                status = Status.epa
+            ),
+                Message.Val(old.pdAuth!!)
+            )
+        },
+        pre = { m ->
+            old.pdAuth != null &&
+                old.status == Status.epr &&
+                m == Message.Req(old.pdAuth!!)
+        },
+        post = { m, result ->
+            val (dash, mr) = result
+            xiConPurseReq(old, dash) &&
+                dash.balance == old.balance - old.pdAuth!!.value &&
+                dash.status == Status.epa &&
+                mr == Message.Val(old.pdAuth!!)
+        }
+    )
+
+    val authenticValMessage = function(
+        command = { m: Message ->
+            old
+        },
+        pre = { m ->
+            m == Message.Val(old.pdAuth!!)
+        }
+    )
+
+    private val xiConPurseVal = function(
+        command = { before: ConPurse, after: ConPurse ->
+            //before.balance == after.balance &&
+                before.exLog == after.exLog &&
+                before.name == after.name &&
+                before.nextSeqNo == after.nextSeqNo &&
+                before.pdAuth == after.pdAuth //&&
+                //before.status == after.status
+        }
+    )
+
+    val valPurseOkay = function(
+        command = { m: Message ->
+            val dash = authenticValMessage(m)
+            mk_(old.transform(
+                balance = old.balance + old.pdAuth!!.value,
+                status = Status.eaTo
+            ), Message.Ack(old.pdAuth!!))
+        },
+        pre = { m ->
+            old.pdAuth != null &&
+                old.status == Status.epv &&
+                m == Message.Val(old.pdAuth!!)
+        },
+        post = { m, result ->
+            val (dash, mr) = result
+            xiConPurseVal(old, dash) &&
+                dash.balance == old.balance + old.pdAuth!!.value &&
+                dash.status == Status.eaTo &&
+                mr == Message.Ack(old.pdAuth!!)
+        }
+    )
+
+    val authenticAckMessage = function(
+        command = { m: Message ->
+            old
+        },
+        pre = { m ->
+            m == Message.Ack(old.pdAuth!!)
+        }
+    )
+
+    private val xiConPurseAck = function(
+        command = { before: ConPurse, after: ConPurse ->
+            before.balance == after.balance &&
+            before.exLog == after.exLog &&
+                before.name == after.name &&
+                before.nextSeqNo == after.nextSeqNo //&&
+                //before.pdAuth == after.pdAuth &&
+            //before.status == after.status
+        }
+    )
+
+    val ackPurseOkay = function(
+        command = { m: Message ->
+            val dash = authenticAckMessage(m)
+            mk_(old.transform(
+                status = Status.eaFrom
+            ), Message.Bottom)
+        },
+        pre = { m ->
+            old.pdAuth != null &&
+                old.status == Status.epa &&
+                m == Message.Ack(old.pdAuth!!)
+        },
+        post = { m, result ->
+            val (dash, mr) = result
+            xiConPurseAck(old, dash) &&
+                dash.status == Status.eaFrom &&
+                mr == Message.Bottom
+        }
+    )
+
+    val readExceptionLogPurseEaFromOkay = function(
+        command = { m: Message ->
+            val mr = if (old.exLog.isEmpty()) Message.Bottom
+                else Message.ExceptionLogResult(old.name, old.exLog.arbitrary())
+            mk_(old, mr)
+        },
+        pre = { m ->
+            old.status == Status.eaFrom &&
+                m == Message.ReadExceptionLog
+        },
+        post = { m, result ->
+            val (dash, mr) = result
+            dash == old && (
+                mr == Message.Bottom || mr in dash.exLog.map { ld -> Message.ExceptionLogResult(dash.name, ld) })
+        }
+    )
+
+    val readExceptionLogPurseOkay = function(
+        command = { m: Message ->
+            val (dash, _) = abortPurseOkay(m)
+            dash.functions.readExceptionLogPurseEaFromOkay(m)
+        },
+        pre = { m ->
+            abortPurseOkay.pre(m) &&
+                readExceptionLogPurseEaFromOkay.pre(m)
+        },
+        post = { m, result ->
+            val (middle, mm) = abortPurseOkay(m)
+            val (dash, mr) = result
+            abortPurseOkay.post(m, mk_(middle, mm)) &&
+                middle.functions.readExceptionLogPurseEaFromOkay.post(m, result)
+        }
+    )
+
+    private val xiConPurseClear = function(
+        command = { before: ConPurse, after: ConPurse ->
+            before.balance == after.balance &&
+                //before.exLog == after.exLog &&
+                before.name == after.name &&
+                before.nextSeqNo == after.nextSeqNo &&
+                before.pdAuth == after.pdAuth &&
+                before.status == after.status
+        }
+    )
+
+    val clearExceptionLogPurseEaFromOkay = function(
+        command = { m: Message ->
+            mk_(old.transform(
+                exLog = emptySet<PayDetails>()
+            ), Message.Bottom
+            )
+        },
+        pre = { m ->
+            old.status == Status.eaFrom &&
+                old.exLog.isNotEmpty() &&
+                m == Message.ExceptionLogClear(old.name, image(as_Set1(old.exLog)))
+        },
+        post = { m, result ->
+            val (dash, mr) = result
+            xiConPurseClear(old, dash) &&
+                dash.exLog.isEmpty() &&
+                mr == Message.Bottom
+        }
+    )
+
+    val clearExceptionLogPurseOkay = function(
+        command = { m: Message ->
+            val (dash, _) = abortPurseOkay(m)
+            dash.functions.clearExceptionLogPurseEaFromOkay(m)
+        },
+        pre = { m ->
+            abortPurseOkay.pre(m) &&
+                old.exLog.isNotEmpty() &&
+                m == Message.ExceptionLogClear(old.name, image(as_Set1(old.exLog).union(
+                    if (old.status in mk_Set(Status.epv, Status.epa)) mk_Set(old.pdAuth) else emptySet()
+            )))
+        },
+        post = { m, result ->
+            val (middle, mm) = abortPurseOkay(m)
+            val (dash, mr) = result
+            abortPurseOkay.post(m, mk_(middle, mm)) &&
+                middle.functions.clearExceptionLogPurseEaFromOkay.post(m, result)
         }
     )
 }
